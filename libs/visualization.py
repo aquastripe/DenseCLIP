57576c4fc4fc589987dd81b1f69084d9cce5fcbc
Hao-Ting Li <maniac@csie.io>
2022-01-28 02:18:26 +0800

    feat: add `plot_image_grid()`

--- libs/visualization.py
+++ libs/visualization.py
@@ -8,3 +8,14 @@ def plot_images(images, filename, figsize=(7.1, 2.41)):
         ax[i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
     fig.savefig(filename)
+
+
+def plot_image_grid(image_grid, filename, figsize=(14.40, 14.40)):
+    fig, ax = plt.subplots(nrows=len(image_grid), ncols=7, figsize=figsize, tight_layout=True)
+    for row in range(len(image_grid)):
+        for col in range(7):
+            ax[row, col].imshow(image_grid[row][col].image)
+            ax[row, col].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[],
+                             xlabel=image_grid[row][col].x_label)
+
+    fig.savefig(filename, bbox_inches='tight')
